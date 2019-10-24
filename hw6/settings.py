e9d514559f32ff1cc49017bfb9f71cd7f13540c0
Kelly Deng <kelly.deng@stonybrook.edu>
2019-10-24 12:44:31 -0400

    Added app to list of installed apps

--- hw6/settings.py
+++ hw6/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django_cassandra_engine',
+    'cassandra_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
